23937f74cf476c486312724eff679ba034baa975
jhkim593 <fpdlwjzlr@naver.com>
2025-09-15 02:00:41 +0900

    공통 예외 처리 로직 추가

---
+++ common/src/main/java/jhkim593/springboard/common/error/BaseControllerAdvice.java
@@ -0,0 +1,37 @@
+package jhkim593.springboard.common.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@Slf4j
+public class BaseControllerAdvice {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Map<String, Object>> handleCustomException(CustomException e) {
+        log.warn("Custom Exception occurred: {}", e.getMessage(), e);
+
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("code", e.getErrorCode().getCode());
+        errorResponse.put("message", e.getErrorCode().getMessage());
+
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGenericException(Exception e) {
+        log.error("Unexpected Exception occurred: {}", e.getMessage(), e);
+
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("code", "E999");
+        errorResponse.put("message", "Internal Server Error");
+
+        return ResponseEntity.status(500).body(errorResponse);
+    }
+}
---
+++ common/src/main/java/jhkim593/springboard/common/error/BaseErrorCode.java
@@ -0,0 +1,7 @@
+package jhkim593.springboard.common.error;
+
+public interface BaseErrorCode {
+    String getCode();
+    String getMessage();
+    int getStatus();
+}
