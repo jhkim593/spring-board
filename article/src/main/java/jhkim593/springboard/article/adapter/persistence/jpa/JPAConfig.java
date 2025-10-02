a68639bd12db32052f8471984e8a959509b9d3f4
jhkim593 <jhkim593@sparrow.im>
2025-10-02 10:33:10 +0900

    jpa common bean 설정 위치 수정

--- article/src/main/java/jhkim593/springboard/article/adapter/persistence/jpa/JPAConfig.java
+++ article/src/main/java/jhkim593/springboard/article/adapter/persistence/jpa/JPAConfig.java
@@ -3,12 +3,16 @@ package jhkim593.springboard.article.adapter.persistence.jpa;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = { "jhkim593.springboard.common.outbox", "jhkim593.springboard.article"})
+@EntityScan(basePackages = {"jhkim593.springboard.common.outbox","jhkim593.springboard.article"})
 public class JPAConfig {
     @PersistenceContext
     private EntityManager entityManager;
