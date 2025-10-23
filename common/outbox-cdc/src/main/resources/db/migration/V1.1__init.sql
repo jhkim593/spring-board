f123b00a9507a174fe6894b7c8f54ec5aec6e46d
jhkim593 <jhkim593@sparrow.im>
2025-10-23 16:54:23 +0900

    outbox cdc event ddl 변경

--- common/outbox-cdc/src/main/resources/db/migration/V1.1__init.sql
+++ common/outbox-cdc/src/main/resources/db/migration/V1.1__init.sql
@@ -1,16 +1,11 @@
 CREATE TABLE outbox_event (
     id BIGINT PRIMARY KEY,
-    event_type VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
     aggregate_id BIGINT NOT NULL,
-    message TEXT NOT NULL,
-    published BOOLEAN NOT NULL DEFAULT FALSE,
+    payload TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
 
 CREATE PUBLICATION outbox_publication
 FOR TABLE outbox_event
 WITH (publish = 'insert');
-
--- CREATE INDEX idx_article_event_article_id ON article_event(article_id);
--- CREATE INDEX idx_article_event_published ON article_event(published);
--- CREATE INDEX idx_article_event_created_at ON article_event(created_at);
